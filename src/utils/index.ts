f458ecdcea06823afe996b01a3adf0fb21f28489
Colby Blaisdell <ccblaisdell@gmail.com>
2023-08-29 15:39:41 -0500

    Refactor utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
-export * from "./times";
 export * from "./opportunity";
 export type { Opportunity } from "./opportunity";
+
+export const times = <T>(n: number, fn: (i: number) => T) => {
+  return Array.from(Array(n)).map((_, i) => fn(i));
+};
+
+export const mergeProps = (...propSets: Record<string, any>[]) => {
+  return propSets.reduce((prev, next) => {
+    return {
+      ...prev,
+      className: [prev.className, next.className].filter(Boolean).join(" "),
+      style: {
+        ...prev.style,
+        ...next.style,
+      },
+    };
+  }, {});
+};
--- src/utils/times.ts
+++
@@ -1,3 +0,0 @@
-export const times = <T>(n: number, fn: (i: number) => T) => {
-  return Array.from(Array(n)).map((_, i) => fn(i));
-};
